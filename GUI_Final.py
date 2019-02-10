3690aabdc613ac1028dfc6aee0b73c5562c6742a
Andy Wu <andywu0408@gmail.com>
2019-02-10 10:01:53 -0800

    fk

--- GUI_Final.py
+++ GUI_Final.py
@@ -7,7 +7,7 @@ import threading
 import NLP
 import PIL
 from PIL import Image, ImageTk
-from Object_detection_webcam import arm_detect
+#from Object_detection_webcam import arm_detect
 
 WIDTH = 640 * 2
 HEIGHT = 533.33 * 2
@@ -140,21 +140,9 @@ def chapter2():
     lower_frame = Frame(root, bd=10)
     lower_frame.place(relx=0.5, rely=0.9, relwidth=1, relheight=0.1, anchor="n")
 
-    start_button = Button(lower_frame, image=pause_image, relief="flat", borderwidth=0)
-    start_button.place(relx=0.05, rely=0, relheight=1)
 
-    # pause_image = PhotoImage(file="./Source_Image/pause.png")
-    pause_button = Button(lower_frame, image=pause_image, relief="flat", borderwidth=0)
-    pause_button.place(relx=0.1, rely=0, relheight=1)
-
-    resume_button = Button(lower_frame, image=pause_image, relief="flat", borderwidth=0)
-    resume_button.place(relx=0.15, rely=0, relheight=1)
-
-    # stop_image = PhotoImage(file="./Source_Image/stop.png")
     stop_button = Button(lower_frame, image=stop_image, relief="flat", command=lambda: (root.destroy(), chapter3()))
     stop_button.place(relx=0.2, rely=0, relheight=1)
-
-    # screenshot_image = PhotoImage(file="./Source_Image/screenshot.png")
     screenshot_button = Button(lower_frame, image=screenshot_image, relief="flat")
     screenshot_button.place(relx=0.9, rely=0, relheight=1)
 
