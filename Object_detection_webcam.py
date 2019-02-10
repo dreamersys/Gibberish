0afa251276c2d982ef18e394e43ae632a8dfe4e0
David Guo <dvguo@ucdavis.edu>
2019-02-10 06:44:03 -0800

    speed plot ver.1

--- Object_detection_webcam.py
+++ Object_detection_webcam.py
@@ -4,6 +4,7 @@ import numpy as np
 import tensorflow as tf
 import math
 import time
+import matplotlib.pyplot as plt
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
@@ -53,7 +54,9 @@ def arm_detect(frame, prev_time, prev_avg_cood):
         delta = math.sqrt(
           (current_avg_cood[0] - prev_avg_cood[0]) ** 2 + (current_avg_cood[1] - prev_avg_cood[1]) ** 2) * 2.54 / 96
         speed = delta / (time.clock() - prev_time)
-        print(speed)
+        plt.plot(time.clock(), speed)
+        plt.show()
+        #print(speed)
     else:
         current_avg_cood = prev_avg_cood
 
