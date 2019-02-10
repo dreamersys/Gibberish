155354def13086f9026598e24eb6ba0a31bfa420
Jerry Kuo <jerrykuo820@gmail.com>
2019-02-09 22:20:59 -0800

    Update GUI_MainFrame.py

--- GUI_MainFrame.py
+++ GUI_MainFrame.py
@@ -9,8 +9,8 @@ root = Tk()
 root.title("Gibberish")
 timer = time.clock()
 video = cv2.VideoCapture(0)
-WIDTH = 1280
-HEIGHT = 720
+WIDTH = 640*2
+HEIGHT = 533.33*2
 
 canvas = Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
@@ -25,16 +25,16 @@ lower_frame = Frame(root, bd=10)
 lower_frame.place(relx=0.5, rely=0.9,relwidth = 1,relheight = 0.1, anchor="n")
 
 pause_image=PhotoImage(file="./Source_Image/pause.png")
-pause_button=Button(lower_frame,image=pause_image, relief=SUNKEN, bg="#B3B3B3", borderwidth=0)
+pause_button=Button(lower_frame,image=pause_image, relief="flat", borderwidth=0)
 pause_button.place(relx=0.05,rely=0, relheight=1)
 
 stop_image=PhotoImage(file="./Source_Image/stop.png")
-stop_button=Button(lower_frame,image=stop_image, relief=SUNKEN, bg="#B3B3B3")
-stop_button.place(relx=0.1,rely=0, relheight=1)
+stop_button=Button(lower_frame,image=stop_image, relief="flat")
+stop_button.place(relx=0.1, rely=0, relheight=1)
 
 screenshot_image=PhotoImage(file="./Source_Image/screenshot.png")
-screenshot_button=Button(lower_frame,image=screenshot_image, relief=SUNKEN, bg="#B3B3B3")
-screenshot_button.place(relx=0.9,rely=0, relheight=1)
+screenshot_button=Button(lower_frame,image=screenshot_image, relief="flat")
+screenshot_button.place(relx=0.9, rely=0, relheight=1)
 
 wave_label = Label(lower_frame, text="HI THERE", bg="#ffffff")
 wave_label.place(relx=0.5, rely=0.25)
